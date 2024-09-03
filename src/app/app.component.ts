e710c6907a31544cd6ab68f0e72e4eef8f0b809b
root <root@Elian-D>
2024-09-03 15:17:51 -0400

    rendering the app-roles component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { RolesComponent } from './components/roles/roles.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RolesComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
